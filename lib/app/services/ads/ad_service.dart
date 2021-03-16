302a76a2e9bbf32036f0e2d8751b3c7c671e5c0d
roma7481 <roma7481@gmail.com>
2021-03-15 22:23:33 -0400

    localization

--- lib/app/services/ads/ad_service.dart
+++ lib/app/services/ads/ad_service.dart
@@ -2,18 +2,18 @@ import 'dart:io';
 
 import 'package:rune_of_the_day/app/constants/strings/strings.dart';
 
-String testInterestialAppId = 'ca-app-pub-3940256099942544/4411468910';
+// String testInterestialAppId = 'ca-app-pub-3940256099942544/4411468910';
 String realInterestialAppId = 'ca-app-pub-1763151471947181/1800598808';
 
-String testNativeAppId = 'ca-app-pub-3940256099942544/3986624511';
+// String testNativeAppId = 'ca-app-pub-3940256099942544/3986624511';
 String realNativeAppId = 'ca-app-pub-1763151471947181/8366007157';
 
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
-      return testNativeAppId;
+      return realNativeAppId;
     } else if (Platform.isIOS) {
-      return testNativeAppId;
+      return realNativeAppId;
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -31,9 +31,9 @@ class AdManager {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return testInterestialAppId;
+      return realInterestialAppId;
     } else if (Platform.isIOS) {
-      return interAdId;
+      return realInterestialAppId;
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
